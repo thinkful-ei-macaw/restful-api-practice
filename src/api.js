8ac413ed7cf513f3691684300bf4e8502b985733
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-03-11 19:03:21 -0700

    7 of 12 completed

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/andrewb'
+
+function getItems() {
+    return fetch(`${BASE_URL}/items`)
+}
+
+function createItem(name) {
+    const newItem = JSON.stringify({ name });
+    return fetch(`${BASE_URL}/items`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: newItem
+    })
+}
+
+function updateItem(id, updateData) {
+    
+    return fetch(`${BASE_URL}/items/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updateData)
+    })
+}
+
+export default {
+    getItems,
+    createItem,
+    updateItem
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
-
 import shoppingList from './shopping-list';
+import store from './store';
+
 
 const main = function () {
+  
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
